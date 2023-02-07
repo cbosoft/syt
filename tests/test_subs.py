8cb2baa61f05ffd95d1d9445f5057e4e22ae196e
Christopher Boyle <chrismjboyle@gmail.com>
2023-02-07 22:09:53 +0000

    Add test where two nodes have same templated value

--- tests/test_subs.py
+++ tests/test_subs.py
@@ -15,3 +15,11 @@ def test_templating_1():
     cfgs = cfg.apply_template('data/input1.syt')
     assert len(cfgs) == 81
 
+
+def test_templating_2():
+    cfg = CfgNode()
+    cfg.data = CfgNode()
+    cfg.data.spec = None
+    cfg.data.eval_spec = None
+    cfgs = cfg.apply_template('data/input2.syt')
+    assert len(cfgs) == 16
